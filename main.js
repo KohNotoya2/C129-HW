ed1448f4b1a1775a1286f423f1d00ffdaa1c3f01
KohNotoya2 <93630361+KohNotoya2@users.noreply.github.com>
2022-12-28 16:06:28 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+song1="";
+song2="";
+
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
+scoreLeftWrist=0;
+
+song_1status="";
+song_2status="";
+
+function preload() {
+    song1=loadSound("song1.mp3");
+    song2=loadSound("song2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses)
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized");
+}
+
+function draw() {
+    image(video,0,0,600,500);
+
+    song_1status=song1.isPlaying()
+    song_2status=song2.isPlaying();
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreLeftWrist>0.2) {
+        circle(leftWristX,leftWristY,20);
+        song2.stop()
+    }
+
+    if(song_1status==false) {
+        song1.play()
+        document.getElementById("song_name").innerHTML="Song Playing is :Empire State of Mind";
+    }
+}
+
+function gotPoses(results) {
+    if(results.length>0) {
+        console.log(results);
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist ="+scoreLeftWrist);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("LeftWristX = " + leftWristX + "LeftWristY = " + leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("RightWristX = " + rightWristX +"RightWristY = " + rightWristY);
+    }
+}
